a744d8eb34efad98f39aee93ae3681c06c52c3aa
NahuelGalera <n.galera@alumno.um.edu.ar>
2024-04-30 15:31:12 -0300

    Orientado Objetos Nahuel

---
+++ main.py
@@ -0,0 +1,9 @@
+from persona import Persona
+
+if __name__ == '__main__':
+     persona = Persona()
+     print(persona.__dict__)
+     persona = Persona()
+     print(persona.__nombre__, persona.__apellido__, persona.__du__)
+     persona = Persona()
+     print(persona.__nombre__, persona.__apellido__, persona.__du__)
---
+++ persona.py
@@ -0,0 +1,9 @@
+class Persona:
+
+    def __init__(self, nombre: str = "john", apellido: str = "doe", du: int = 123456):
+        self.__nombre__ = nombre
+        self.__apellido__ = apellido
+        self.__du__ = du
+
+    def __str__(self):
+        return f'Mis datos son nombre = {self.__nombre__} apellido = {self.__apellido__} documento = {self.__du__}'
